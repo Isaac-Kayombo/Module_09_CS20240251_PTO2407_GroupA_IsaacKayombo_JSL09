74f7ef71080f87461c10dceba130dc2c9c505f6e
IsaacK47 <isaackayombo41@gmail.com>
2025-01-31 13:22:13 +0200

    Unsplash API for getting background image

---
+++ index.js
@@ -0,0 +1,6 @@
+// UNSPLASH API FOR GETTING BACKGROUND IMAGE
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.full})`
+    })
